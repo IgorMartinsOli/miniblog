b10547b45ec67edc4c04ddbe2ee6de10ddb51cb1
Igor Martins <igor.martins@smartnx.com>
2024-03-06 10:03:16 -0300

    Criando integração com o firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWuRADm5kRwGuuBAaI1MRvl4B5QnXXQUM",
+    authDomain: "miniblog-31b21.firebaseapp.com",
+    projectId: "miniblog-31b21",
+    storageBucket: "miniblog-31b21.appspot.com",
+    messagingSenderId: "716712109322",
+    appId: "1:716712109322:web:d53f9ba04b78c3caa706f3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFireStore(app);
+
+export {db};
